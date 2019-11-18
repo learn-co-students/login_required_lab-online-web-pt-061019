99543330ee87d999b4109c284925491a914dd411
Chris Junker <elevatemedfit@gmail.com>
2019-11-18 16:50:52 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/home' => 'sessions#home'
+
+  get '/new' => 'sessions#new'
+  post '/create' => 'sessions#create'
+  post '/destroy' => 'sessions#destroy'
+  get '/show' => 'secrets#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
