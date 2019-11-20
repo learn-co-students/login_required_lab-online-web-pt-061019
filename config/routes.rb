c3add8950994d64a9514390ef7a552dd0140a90a
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-19 16:50:53 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/secret', to: 'secrets#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/logout', to: "sessions#destroy"
+  get '/logout', to: 'sessions#destroy'
 end
