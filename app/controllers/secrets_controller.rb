0376508f58512b134b524289637d4eb36ea2f3cb
maasa333 <konomaasa@gmail.com>
2019-11-20 20:47:55 -0800

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,11 @@
+require 'pry'
 class SecretsController < ApplicationController
     def show
-        
+        if current_user 
+            render "/welcome"
+        else
+            current_user == nil || ""
+            redirect_to "/sessions/new"
+        end
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,15 @@
 class SessionsController < ApplicationController
+    def new
+    end
 
+    def create
+        # current_user = params[:name]
+        
+        if params[:name] == nil || ""
+            redirect_to "/"
+        else
+            session[:name] = params[:name]
+            redirect_to "/welcome"
+        end
+    end
 end
