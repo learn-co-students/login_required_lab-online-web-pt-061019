70223317e3c64796960f50fe1c07fd3cc9dad184
Brittany Javalera <brittjavalera@gmail.com>
2019-11-21 07:59:12 -0800

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -3,4 +3,9 @@ class SecretsController < ApplicationController
 
     def show
     end
+
+    private
+    def require_login
+        redirect_to '/login' unless session.include? :name
+    end
 end
