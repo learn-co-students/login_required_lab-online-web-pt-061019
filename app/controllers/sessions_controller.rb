2b24c0fcc7b2602df7fa459ab162172b5bee9c3b
maasa333 <konomaasa@gmail.com>
2019-11-21 19:10:57 -0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
 
     def create
         if !params[:name] || params[:name].empty?
-            redirect_to controller: "sessions", action: "new"
+            # redirect_to controller: "sessions", action: "new"
+            redirect_to "/sessions/new"
         else 
             session[:name] = params[:name]
         end
