4554faa81512b88c412495430dfda20e40d221e4
maasa333 <konomaasa@gmail.com>
2019-11-21 19:09:04 -0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,14 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if current_user.present?
-            current_user = params[:name]
+        if !params[:name] || params[:name].empty?
+            redirect_to controller: "sessions", action: "new"
         else 
-            current_user == nil
-            redirect_to "/sessions/new"
+            session[:name] = params[:name]
         end
+    end
+
+    def destroy
+        session.delete :name
     end
 end
